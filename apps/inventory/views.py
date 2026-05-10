964194603ebb17fc4ed4b30da2a28e9fc3bf8b3b
YalukR <yalukramos@gmail.com>
2026-05-09 21:50:57 -0600

    softdelete categorias

--- apps/inventory/views.py
+++ apps/inventory/views.py
@@ -1,40 +1,46 @@
-# apps/inventory/views.py
 from rest_framework import viewsets, permissions
+from rest_framework.exceptions import ValidationError
 from .models import Categoria, Producto, Movimiento
 from .serializers import CategoriaSerializer, ProductoSerializer, MovimientoSerializer
+
 
 class CategoriaViewSet(viewsets.ModelViewSet):
     serializer_class = CategoriaSerializer
     permission_classes = [permissions.IsAuthenticated]
 
     def get_queryset(self):
-        return Categoria.objects.filter(
-            tenant=self.request.user.tenant
-        )
+        return Categoria.objects.filter(tenant=self.request.user.tenant)
 
     def perform_create(self, serializer):
         serializer.save(tenant=self.request.user.tenant)
+
+    def perform_destroy(self, instance):
+        instance.productos.update(activo=False)
+        instance.activo = False
+        instance.save()
+
 
 class ProductoViewSet(viewsets.ModelViewSet):
     serializer_class = ProductoSerializer
     permission_classes = [permissions.IsAuthenticated]
 
     def get_queryset(self):
-        return Producto.objects.filter(
-            tenant=self.request.user.tenant
-        )
+        return Producto.objects.filter(tenant=self.request.user.tenant)
 
     def perform_create(self, serializer):
         serializer.save(tenant=self.request.user.tenant)
+
+    def perform_destroy(self, instance):
+        instance.activo = False
+        instance.save()
+
 
 class MovimientoViewSet(viewsets.ModelViewSet):
     serializer_class = MovimientoSerializer
     permission_classes = [permissions.IsAuthenticated]
 
     def get_queryset(self):
-        return Movimiento.objects.filter(
-            producto__tenant=self.request.user.tenant
-        )
+        return Movimiento.objects.filter(producto__tenant=self.request.user.tenant)
 
     def perform_create(self, serializer):
         serializer.save(usuario=self.request.user)
