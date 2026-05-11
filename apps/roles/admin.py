463709af1213f391438ca5c28af8838870d73538
YalukR <yalukramos@gmail.com>
2026-05-11 15:37:09 -0600

    modelos y admin actualizados

--- apps/roles/admin.py
+++ apps/roles/admin.py
@@ -2,19 +2,25 @@
 from django.contrib import admin
 from .models import Permiso, Rol, RolPermiso
 
+class RolPermisoInline(admin.TabularInline):
+    model = RolPermiso
+    extra = 1
+
 @admin.register(Permiso)
 class PermisoAdmin(admin.ModelAdmin):
-    list_display = ('codigo', 'modulo', 'descripcion')
-    list_filter = ('modulo',)
-    search_fields = ('codigo',)
+    list_display = ('codigo', 'modulo', 'submodulo', 'ruta', 'icono', 'descripcion')
+    list_filter = ('modulo', 'submodulo')
+    search_fields = ('codigo', 'descripcion')
+    ordering = ('modulo', 'submodulo', 'codigo')
 
 @admin.register(Rol)
 class RolAdmin(admin.ModelAdmin):
     list_display = ('nombre', 'tenant', 'activo')
     list_filter = ('activo',)
     search_fields = ('nombre', 'tenant__nombre_negocio')
-    
+    inlines = [RolPermisoInline]  # ver/editar permisos del rol inline
+
 @admin.register(RolPermiso)
 class RolPermisoAdmin(admin.ModelAdmin):
     list_display = ('rol', 'permiso')
-    list_filter = ('rol', 'permiso')
+    list_filter = ('rol__tenant', 'permiso__modulo')
--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,10 +1,10 @@
 # apps/users/admin.py
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
 from .models import Usuario
 
 @admin.register(Usuario)
 class UsuarioAdmin(admin.ModelAdmin):
     list_display = ('email', 'tenant', 'rol', 'activo', 'ultimo_acceso')
-    list_filter = ('activo', 'tenant')
+    list_filter = ('activo', 'tenant', 'rol')
     search_fields = ('email', 'tenant__nombre_negocio')
+    readonly_fields = ('ultimo_acceso',)
